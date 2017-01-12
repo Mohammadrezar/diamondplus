fa6145694b4fcc95037cf74f247fb5218556471c
AmirSbss <mrahs@emeil.ir>
2017-01-12 23:39:29 +0330

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+if [ ! -f ./tg ]; then
+  wget "https://valtman.name/files/telegram-cli-1222"
+  mv telegram-cli-1222 ./tg
+  chmod 777 tg
+  ./tg -s bot.lua
+fi
 ./tg -s bot.lua
